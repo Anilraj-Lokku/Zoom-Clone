e26eeef58fcb9fa4e333b568e94362bb5670a931
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-04-13 01:51:02 +0530

    NextJs Server Side Added

---
+++ actions/stream.actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAN_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const tokenProvider = async () => {
+ const user = await currentUser();
+
+ if (!user) throw new Error("User is not LoggedIn");
+ if (!apiKey) throw new Error("Stream API KEY Secret Missing");
+ if (!apiSecret) throw new Error("Stream API KEY is missing");
+
+ const streamClient = new StreamClient(apiKey, apiSecret);
+
+ const expirationDate = Math.floor(Date.now() / 1000) + 3600;
+ const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+ const token = streamClient.createToken(user.id, expirationDate, issuedAt);
+ return token;
+};
