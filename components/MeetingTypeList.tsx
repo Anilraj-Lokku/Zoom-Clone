2b69a605f660b970c57afc98d6f20ec9df93be68
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-04-12 18:43:15 +0530

    Dialog Box Component Using ShadCn Added

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,19 +4,22 @@ import Image from "next/image";
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModel";
 
 const MeetingTypeList = () => {
  const router = useRouter();
  const [meetingState, setMeetingState] = useState<
   "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
  >();
+
+ const createMeeting = () => {};
  return (
   <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
    <HomeCard
     img="/icons/add-meeting.svg"
     title="New Meeting"
     description="Start an instant meeting"
-    handleClick={() => setMeetingState("isJoiningMeeting")}
+    handleClick={() => setMeetingState("isInstantMeeting")}
     className="bg-orange-1"
    />
    <HomeCard
@@ -39,6 +42,14 @@ const MeetingTypeList = () => {
     description="Join meeting via invitation link"
     handleClick={() => router.push("/recordings")}
     className="bg-yellow-1"
+   />
+   <MeetingModel
+    isOpen={meetingState === "isInstantMeeting"}
+    onClose={() => setMeetingState(undefined)}
+    title="Start an instant meeting"
+    className="text-center"
+    buttonText="Start Meeting"
+    handleClick={createMeeting}
    />
   </section>
  );
